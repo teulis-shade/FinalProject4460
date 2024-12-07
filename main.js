b9382d48577691c65053bd6e8f4470c946a6d5b2
Teulis <tziegler30@gatech.edu>
2024-12-06 21:01:41 -0500

    Added scales

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // This function converts strings to numeric temperatures during data preprocessing
 function dataPreprocessor(row) {
+
+    //if the blue team won, use blue team as winning team, otherwise use red.
     if (row.blueWins == 1) {
         return {
             winFirstBlood: row.blueFirstBlood == 1,
@@ -68,20 +70,32 @@ var chartHeight = svgHeight - padding.t - padding.b;
 var chartG = svg.append('g')
     .attr('transform', 'translate('+[padding.l, padding.t]+')');
 
-// Compute the spacing for bar bands based on the number of countries (20 in this case)
-var barBand = chartHeight / 20;
-var barHeight = barBand * 0.7;
+//Create the scales for values
+var firstBloodScale = d3.scaleLinear([30, chartHeight - 30], [0, 1]);
+var goldDiffScale = d3.scaleLinear([30, chartHeight - 30], [-20000, 20000]);
+var totalGoldScale = d3.scaleLinear([30, chartHeight - 30], [0, 20000]);
+var killScale = d3.scaleLinear([30, chartHeight - 30], [0, 20]);
+var deathScale = d3.scaleLinear([30, chartHeight - 30], [0, 20]);
+var assistsScale = d3.scaleLinear([30, chartHeight - 30], [0, 40]);
+var csScale = d3.scaleLinear([30, chartHeight - 30], [0, 300]);
+var towerScale = d3.scaleLinear([30, chartHeight - 30], [0, 5]);
+var wardScale = d3.scaleLinear([30, chartHeight - 30], [0, 50]);
+var lvlScale = d3.scaleLinear([30, chartHeight - 30], [1, 18]);
+
+
 
 var allGames;
 
 d3.csv('high_diamond_ranked_10min.csv', dataPreprocessor).then(function(dataset) {
+    allGames = dataset;
 
-    allGames = dataset
-    // **** Your JavaScript code goes here ****
     updateChart();
 });
 
 function updateChart() {
+    // clear the old stuff
+
+
     // Update the chart
     var firstBlood = document.getElementById("fbCheck").checked == true;
     var goldDiff = document.getElementById("goldDiffCheck").checked == true;
@@ -95,34 +109,76 @@ function updateChart() {
     var level = document.getElementById("avgLvlCheck").checked == true;
 
     allGames.forEach(element => {
+        
+        // Compute the spacing for axes based on number of categories selected
+        var axisBand = chartHeight / (+firstBlood + +goldDiff + +gold + +kill + +death + +assist + +cs + +tower + +ward + +level);
+        
+        // Make a variable to store how far on the PCP we are
+        var numberUsed = 0;
+        
+        // Create a variable for the previous line
+        var previousDot = null;
+
+        //use this to set the class of the lines to change the css
+        var className = ".redWins";
+        if (winColor == "blue") {
+            className = ".blueWins";
+        }
         if (firstBlood) {
             console.log("firstBlood: " + element.winFirstBlood + " " + element.loseFirstBlood);
         }
         if (goldDiff) {
+            if (previousDot == null) {
+                previousDot;
+            }
             console.log("goldDiff: " + element.winGoldDiff + " " + element.loseGoldDiff);
         }
         if (gold) {
+            if (previousDot == null) {
+                previousDot;
+            }
             console.log("totalGold: " + element.winTotalGold + " " + element.loseTotalGold);
         }
         if (kill) {
+            if (previousDot == null) {
+                previousDot;
+            }
             console.log("kills: " + element.winKills + " " + element.loseKills);
         }
         if (death) {
+            if (previousDot == null) {
+                previousDot;
+            }
             console.log("deaths: " + element.winDeaths + " " + element.loseDeaths);
         }
         if (assist) {
+            if (previousDot == null) {
+                previousDot;
+            }
             console.log("assists: " + element.winAssists + " " + element.loseAssists);
         }
         if (cs) {
+            if (previousDot == null) {
+                previousDot;
+            }
             console.log("cs: " + element.winCS + " " + element.loseCS);
         }
         if (tower) {
+            if (previousDot == null) {
+                previousDot;
+            }
             console.log("towers taken: " + element.winTowers + " " + element.loseTowers);
         }
         if (ward) {
+            if (previousDot == null) {
+                previousDot;
+            }
             console.log("wards placed: " + element.winWards + " " + element.loseWards);
         }
         if (level) {
+            if (previousDot == null) {
+                previousDot;
+            }
             console.log("average level: " + element.winAvgLvl + " " + element.loseAvgLvl);
         }
     });
